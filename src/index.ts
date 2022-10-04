d49ffa6f43610d390689d1d45f05470ac8d73bd4
marcin <71821642+MarcinPloch@users.noreply.github.com>
2022-10-04 16:05:52 +0200

    timer first aproach

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,29 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const placeholder = document.querySelector('[time-to-close]');
+  const closing1 = 19;
+  const closing2 = 18;
+
+  const day: number = new Date().getDay();
+  const time: number = new Date().getHours();
+
+  function refresh() {
+    let toclose = 0;
+    if (day === 6 || day === 7) {
+      toclose = closing2 - time;
+    }
+    toclose = closing1 - time;
+
+    if (!placeholder) {
+      return;
+    }
+    if (toclose < 6) {
+      placeholder.innerHTML = `There is ${toclose} hours left to close!`;
+    } else {
+      placeholder.innerHTML = ``;
+    }
+  }
+  // setInterval(refresh, 1);
+  document.addEventListener('DOMContentLoaded', refresh);
 });
